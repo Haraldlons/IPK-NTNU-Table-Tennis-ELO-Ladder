d5d6a584d8ad1d1a584876e017fef623a41d0b53
Harald Lønsethagen <haraldlons@gmail.com>
2018-01-15 18:10:29 +0100

    add demo

---
+++ Demo/test_if_you_have_everything_setup.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from datetime import datetime
+import time
+import os
+
+import sys
+from shutil import copyfile
+
+print("You successfully passed the test! Your environment is setup correctly!")
